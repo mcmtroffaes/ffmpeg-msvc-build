05dbd501afc594634a39849d9f8b82d7d1f72acd
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-05-18 14:12:29 +0100

    Add env vars for linkage, license, configuration, and runtime variations.

--- build.sh
+++ build.sh
@@ -1,12 +1,66 @@
+# print commands for debugging
+set -x
+
+OPTIONS="--disable-programs --disable-doc --enable-runtime-cpudetect"
+
+case "$LINKAGE" in
+	Dynamic)
+		OPTIONS+=" --disable-static --enable-shared"
+		;;
+	Static)
+		OPTIONS+=" --enable-static --disable-shared"
+		;;
+	*)
+		echo "LINKAGE must be Dynamic or Static"
+		exit 1
+esac
+
+case "$LICENSE" in
+	LGPL2)
+		;;
+	LGPL3)
+		OPTIONS+="  --enable-version3"
+		;;
+	GPL2)
+		OPTIONS+=" --enable-gpl"
+		;;
+	GPL3)
+		OPTIONS+=" --enable-gpl --enable-version3"
+		;;
+	*)
+		echo "LICENSE must be LGPL2, LGPL3, GPL2, or GPL3"
+		exit 1
+esac
+
+# note: bash is case sensitive; appveyor sets $Configuration instead of $CONFIGURATION
+case "$Configuration" in
+	Release)
+		OPTIONS+=" --extra-cflags=-${RUNTIME_LIBRARY} --extra-cxxflags=-${RUNTIME_LIBRARY}"
+		;;
+	Debug)
+		OPTIONS+=" --extra-cflags=-${RUNTIME_LIBRARY}d --extra-cxxflags=-${RUNTIME_LIBRARY}d"
+		;;
+	*)
+		echo "CONFIGURATION must be Release or Debug"
+		exit 1
+esac
+
 # print some environment variables for debugging
+set +x
+echo "------------------------------------------------------------------------"
+echo "CONFIGURATION=$Configuration"
+echo "LINKAGE=$LINKAGE"
+echo "LICENSE=$LICENSE"
+echo "RUNTIME_LIBRARY=$RUNTIME_LIBRARY"
+echo "OPTIONS=$OPTIONS"
+echo "------------------------------------------------------------------------"
 echo "PATH=$PATH"
 echo "INCLUDE=$INCLUDE"
 echo "LIB=$LIB"
 echo "LIBPATH=$LIBPATH"
 echo "CL=$CL"
 echo "_CL_=$_CL_"
-
-# print commands for debugging
+echo "------------------------------------------------------------------------"
 set -x
 
 # ensure link.exe is the one from msvc
@@ -19,7 +73,7 @@ cl
 
 # run configure
 cd $APPVEYOR_BUILD_FOLDER/ffmpeg
-./configure --toolchain=msvc --disable-ffmpeg --disable-ffprobe --disable-doc --enable-runtime-cpudetect --disable-static --enable-shared --disable-debug --extra-cflags=-MD --extra-cxxflags=-MD
+./configure --toolchain=msvc $OPTIONS
 
 # print last 30 lines from config log file for debugging
 tail -30 config.log
