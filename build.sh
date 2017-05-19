2d05b7047b0424fad0d071de7d8a350c00016b9c
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-05-19 16:48:59 +0100

    Add ffmpeg as submodule, refactor build script.

--- build.sh
+++ build.sh
@@ -1,125 +1,194 @@
-# print commands for debugging
+# exit immediately upon error
+set -e
+
+ffmpeg_date() {
+	cd ffmpeg
+	git show -s --format=%ci HEAD | sed 's/\([0-9]\{4\}\)-\([0-9][0-9]\)-\([0-9][0-9]\).*/\1\2\3/'
+	cd ..
+}
+
+ffmpeg_hash() {
+	cd ffmpeg
+	git show -s --format=%t HEAD
+	cd ..
+}
+
+# VISUAL_STUDIO
+toolset () {
+	case "$1" in
+		Visual\ Studio\ 2013)
+			echo -n "v120"
+			;;
+		Visual\ Studio\ 2015)
+			echo -n "v140"
+			;;
+		Visual\ Studio\ 2017)
+			echo -n "v141"
+			;;
+		*)
+			return 1
+	esac
+}
+
+# LICENSE
+license_file () {
+	case "$1" in
+		LGPL21)
+			echo "COPYING.LGPLv2.1"
+			;;
+		LGPL3)
+			echo "COPYING.LGPLv3"
+			;;
+		GPL2)
+			echo "COPYING.GPLv2"
+			;;
+		GPL3)
+			echo "COPYING.GPLv3"
+			;;
+		*)
+			return 1
+	esac
+}
+
+# LICENSE
+ffmpeg_options_license() {
+	case "$1" in
+		LGPL21)
+			;;
+		LGPL3)
+			echo "--enable-version3"
+			;;
+		GPL2)
+			echo "--enable-gpl"
+			;;
+		GPL3)
+			echo "--enable-gpl --enable-version3"
+			;;
+		*)
+			return 1
+	esac
+}
+
+# LINKAGE
+ffmpeg_options_linkage() {
+	case "$1" in
+		shared)
+			echo "--disable-static --enable-shared"
+			;;
+		static)
+			echo "--enable-static --disable-shared"
+			;;
+		*)
+			return 1
+	esac
+}
+
+# RUNTIME_LIBRARY CONFIGURATION
+cflags_runtime() {
+	echo -n "-$1" | tr '[:lower:]' '[:upper:]'
+	case "$2" in
+		Release)
+			echo ""
+			;;
+		Debug)
+			echo "d"
+			;;
+		*)
+			return 1
+	esac
+}
+
+# RUNTIME_LIBRARY CONFIGURATION
+ffmpeg_options_runtime() {
+	cflags=`cflags_runtime $1 $2`
+	echo "--extra-cflags=$cflags --extra-cxxflags=$cflags"
+}
+
+# PREFIX LICENSE LINKAGE RUNTIME_LIBRARY CONFIGURATION
+ffmpeg_options () {
+	echo -n "--disable-programs --disable-doc --enable-runtime-cpudetect"
+	echo -n " --prefix=$1"
+	echo -n " $(ffmpeg_options_license $2)"
+	echo -n " $(ffmpeg_options_linkage $3)"
+	echo -n " $(ffmpeg_options_runtime $4 $5)"
+}
+
+# LICENSE VISUAL_STUDIO LINKAGE RUNTIME_LIBRARY CONFIGURATION PLATFORM
+target_id () {
+	local ts=$(toolset "$2")
+	echo "ffmpeg-$(ffmpeg_date)-$(ffmpeg_hash)-$1-$ts-$3-$4-$5-$6" | tr '[:upper:]' '[:lower:]'
+}
+
+# assumes we are in the ffmpeg folder
+# PREFIX LICENSE LINKAGE RUNTIME_LIBRARY CONFIGURATION
+function build_ffmpeg() {
+	echo "==============================================================================="
+	echo "build_ffmpeg"
+	echo "==============================================================================="
+	echo "PREFIX=$1"
+	echo "LICENSE=$2"
+	echo "LINKAGE=$3"
+	echo "RUNTIME_LIBRARY=$4"
+	echo "CONFIGURATION=$5"
+	echo "-------------------------------------------------------------------------------"
+	echo "PATH=$PATH"
+	echo "INCLUDE=$INCLUDE"
+	echo "LIB=$LIB"
+	echo "LIBPATH=$LIBPATH"
+	echo "CL=$CL"
+	echo "_CL_=$_CL_"
+	echo "-------------------------------------------------------------------------------"
+
+	# find absolute path for prefix
+	local abs1=$(readlink -f $1)
+
+	# ensure link.exe is the one from msvc
+	mv /usr/bin/link /usr/bin/link1
+	which link
+
+	# ensure cl.exe can be called
+	which cl
+	cl
+
+	# install license file
+	mkdir -p "$abs1/share/doc"
+	cp "ffmpeg/$(license_file $2)" "$abs1/share/doc/ffmpeg-license.txt"
+
+	# run configure and save output (lists all enabled features and mentions license at the end)
+	pushd ffmpeg
+	./configure --toolchain=msvc $(ffmpeg_options $abs1 $2 $3 $4 $5) \
+		> "$abs1/share/doc/ffmpeg-configure.txt"
+	cat "$abs1/share/doc/ffmpeg-configure.txt"
+	#tail -30 config.log  # for debugging
+	make
+	make install
+	popd
+
+	mv /usr/bin/link1 /usr/bin/link
+}
+
+# FOLDER
+function make_zip() {
+	7z a -tzip -r "$1.zip" $1
+}
+
+function appveyor_main() {
+	cd $(cygpath "$APPVEYOR_BUILD_FOLDER")
+	# LICENSE VISUAL_STUDIO LINKAGE RUNTIME_LIBRARY CONFIGURATION PLATFORM
+	local prefix=$(target_id \
+		$LICENSE "$APPVEYOR_BUILD_WORKER_IMAGE" $LINKAGE $RUNTIME_LIBRARY \
+		$Configuration $Platform)
+	# PREFIX LICENSE LINKAGE RUNTIME_LIBRARY CONFIGURATION
+	build_ffmpeg $prefix $LICENSE $LINKAGE $RUNTIME_LIBRARY $Configuration
+	make_zip $prefix
+}
+
+function local_main() {
+	local prefix=$(target_id \
+		$LICENSE "$VISUAL_STUDIO" $LINKAGE $RUNTIME_LIBRARY $CONFIGURATION $PLATFORM)
+	build_ffmpeg $prefix $LICENSE $LINKAGE $RUNTIME_LIBRARY $CONFIGURATION
+	make_zip $prefix
+}
+
 set -x
-BUILD_FOLDER=`cygpath $APPVEYOR_BUILD_FOLDER`
-TARGET="ffmpeg-$FFMPEG_VERSION"
-OPTIONS="--disable-programs --disable-doc --enable-runtime-cpudetect"
-
-TARGET+="-$LICENSE"
-case "$LICENSE" in
-	LGPL21)
-		LICENSE_FILE="COPYING.LGPLv2.1"
-		;;
-	LGPL3)
-		LICENSE_FILE="COPYING.LGPLv3"
-		OPTIONS+=" --enable-version3"
-		;;
-	GPL2)
-		LICENSE_FILE="COPYING.GPLv2"
-		OPTIONS+=" --enable-gpl"
-		;;
-	GPL3)
-		LICENSE_FILE="COPYING.GPLv3"
-		OPTIONS+=" --enable-gpl --enable-version3"
-		;;
-	*)
-		echo "LICENSE must be LGPL21, LGPL3, GPL2, or GPL3"
-		exit 1
-esac
-
-case "$APPVEYOR_BUILD_WORKER_IMAGE" in
-	Visual\ Studio\ 2013)
-		TARGET+="-v120"
-		;;
-	Visual\ Studio\ 2015)
-		TARGET+="-v140"
-		;;
-	Visual\ Studio\ 2017)
-		TARGET+="-v141"
-		;;
-	*)
-		echo "worker image $APPVEYOR_BUILD_WORKER_IMAGE not supported"
-		exit 1
-esac
-
-TARGET+="-$LINKAGE"
-case "$LINKAGE" in
-	shared)
-		OPTIONS+=" --disable-static --enable-shared"
-		;;
-	static)
-		OPTIONS+=" --enable-static --disable-shared"
-		;;
-	*)
-		echo "LINKAGE must be shared or static"
-		exit 1
-esac
-
-# note: bash is case sensitive; need to use $Configuration and $Platform
-TARGET+="-$RUNTIME_LIBRARY"
-TARGET+="-$Configuration"
-TARGET+="-$Platform"
-# ensure RUNTIME_CFLAGS are in upper case (build fails if lower case)
-RUNTIME_CFLAGS=`echo "-$RUNTIME_LIBRARY" | tr '[:lower:]' '[:upper:]'`
-case "$Configuration" in
-	Release)
-		;;
-	Debug)
-		RUNTIME_CFLAGS="${RUNTIME_CFLAGS}d"
-		;;
-	*)
-		echo "CONFIGURATION must be Release or Debug"
-		exit 1
-esac
-OPTIONS+=" --extra-cflags=${RUNTIME_CFLAGS} --extra-cxxflags=${RUNTIME_CFLAGS}"
-
-# lower case
-TARGET=`echo "$TARGET" | tr '[:upper:]' '[:lower:]'`
-
-OPTIONS+=" --prefix=$BUILD_FOLDER/$TARGET"
-
-# print some environment variables for debugging
-set +x
-echo "------------------------------------------------------------------------"
-echo "FFMPEG_VERSION=$FFMPEG_VERSION"
-echo "CONFIGURATION=$Configuration"
-echo "LINKAGE=$LINKAGE"
-echo "LICENSE=$LICENSE"
-echo "RUNTIME_LIBRARY=$RUNTIME_LIBRARY"
-echo "OPTIONS=$OPTIONS"
-echo "------------------------------------------------------------------------"
-echo "PATH=$PATH"
-echo "INCLUDE=$INCLUDE"
-echo "LIB=$LIB"
-echo "LIBPATH=$LIBPATH"
-echo "CL=$CL"
-echo "_CL_=$_CL_"
-echo "------------------------------------------------------------------------"
-set -x
-
-# ensure link.exe is the one from msvc
-rm /usr/bin/link
-which link
-
-# ensure cl.exe can be called
-which cl
-cl
-
-# install license file
-mkdir -p "$BUILD_FOLDER/$TARGET/share/doc"
-cp "$BUILD_FOLDER/ffmpeg-$FFMPEG_VERSION/$LICENSE_FILE" "$BUILD_FOLDER/$TARGET/share/doc/ffmpeg-license.txt"
-
-# run configure and save output (lists all enabled features and mentions license at the end)
-cd "$BUILD_FOLDER/ffmpeg-$FFMPEG_VERSION"
-./configure --toolchain=msvc $OPTIONS > "$BUILD_FOLDER/$TARGET/share/doc/ffmpeg-configure.txt"
-cat "$BUILD_FOLDER/$TARGET/share/doc/ffmpeg-configure.txt"
-
-# print last 30 lines from config log file for debugging
-tail -30 config.log
-
-# run make
-make
-make install
-
-# zip the result
-cd "$BUILD_FOLDER"
-7z a -tzip -r "$TARGET.zip" $TARGET
+appveyor_main
--- install.bat
+++ install.bat
@@ -1,7 +1,5 @@
 @echo on
-curl -o source.tar.xz https://ffmpeg.zeranoe.com/builds/source/ffmpeg/ffmpeg-%FFMPEG_VERSION%.tar.xz
-7z x source.tar.xz
-7z x source.tar
+git submodule update --init --recursive
 mkdir c:\yasm
 set PATH=c:\yasm;%PATH%
 if "%PLATFORM%" == "x64" curl -o "c:\yasm\yasm.exe" http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe
