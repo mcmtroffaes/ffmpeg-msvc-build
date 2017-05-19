b4a91d1640d8cd0f4324bd5f7905f47f95abe7af
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-05-19 23:17:33 +0100

    Increase depth of clone.

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo on
-git submodule update --init --recursive --depth 1
+git submodule update --init --recursive --depth 100
 mkdir c:\yasm
 set PATH=c:\yasm;%PATH%
 if "%PLATFORM%" == "x64" curl -o "c:\yasm\yasm.exe" http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe
