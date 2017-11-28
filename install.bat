d84cb29730dba2d7c0f5ba2dfede437d3a87f413
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-11-28 23:30:28 +0000

    Remove yasm, don't leave nasm.zip in main folder (to avoid artifact).

--- install.bat
+++ install.bat
@@ -1,13 +1,9 @@
 @echo on
 git submodule update --init --recursive --depth 100
-mkdir c:\yasm
-set PATH=c:\yasm;%PATH%
-if "%PLATFORM%" == "x64" curl -o "c:\yasm\yasm.exe" http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe
-if "%PLATFORM%" == "x86" curl -o "c:\yasm\yasm.exe" http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win32.exe
 mkdir c:\nasm
 set PATH=c:\nasm;%PATH%
-if "%PLATFORM%" == "x64" curl -o "nasm.zip" http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win64/nasm-2.13.01-win64.zip
-if "%PLATFORM%" == "x86" curl -o "nasm.zip" http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win32/nasm-2.13.01-win32.zip
-unzip nasm.zip
+if "%PLATFORM%" == "x64" curl -o "c:\nasm.zip" http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win64/nasm-2.13.01-win64.zip
+if "%PLATFORM%" == "x86" curl -o "c:\nasm.zip" http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win32/nasm-2.13.01-win32.zip
+unzip c:\nasm.zip
 move nasm-2.13.01\nasm.exe c:\nasm
 @echo off
