6c2a1d54415ed6dac773db774ee71a1476ace193
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-11-28 15:57:00 +0000

    Install nasm for x264.

--- install.bat
+++ install.bat
@@ -4,4 +4,10 @@ mkdir c:\yasm
 set PATH=c:\yasm;%PATH%
 if "%PLATFORM%" == "x64" curl -o "c:\yasm\yasm.exe" http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe
 if "%PLATFORM%" == "x86" curl -o "c:\yasm\yasm.exe" http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win32.exe
+mkdir c:\nasm
+set PATH=c:\nasm;%PATH%
+if "%PLATFORM%" == "x64" curl -o "nasm.zip" http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win64/nasm-2.13.01-win64.zip
+if "%PLATFORM%" == "x86" curl -o "nasm.zip" http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win32/nasm-2.13.01-win32.zip
+unzip nasm.zip
+move nasm-2.13.01\nasm.exe c:\nasm
 @echo off
