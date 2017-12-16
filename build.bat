ebf5eff234f7966c58c3851931707c8d4719e8a3
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-12-16 18:52:41 +0000

    Cache nasm for more reliability on appveyor.

--- build.bat
+++ build.bat
@@ -1,16 +1,17 @@
 @echo on
-rem set up Visual Studio 2017 64-bit environment for v140 toolset
+rem set up Visual Studio environment for specified toolset and platform
 rem see https://www.appveyor.com/docs/lang/cpp/
 rem and https://stackoverflow.com/a/46994531
 rem (differenes: no need for SDK, use native amd64 instead of x86_amd64)
+if "%TOOLSET%" == "v120" (
+  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+)
 if "%TOOLSET%" == "v140" (
-  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" -vcvars_ver=14.0
-  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat" -vcvars_ver=14.0
+  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 )
-if "%TOOLSET%" == "v141" (
-  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" -vcvars_ver=14.1
-  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat" -vcvars_ver=14.1
-)
+if "%TOOLSET%" == "v141" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %PLATFORM% -vcvars_ver=14.1
 rem run main build script
 C:\msys64\usr\bin\bash -lc "$APPVEYOR_BUILD_FOLDER/build.sh"
 @echo off
--- install.bat
+++ install.bat
@@ -1,9 +1,7 @@
 @echo on
 git submodule update --init --recursive --depth 100
-mkdir c:\nasm
-set PATH=c:\nasm;%PATH%
-if "%PLATFORM%" == "x64" curl -o "c:\nasm.zip" http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win64/nasm-2.13.01-win64.zip
-if "%PLATFORM%" == "x86" curl -o "c:\nasm.zip" http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win32/nasm-2.13.01-win32.zip
-unzip c:\nasm.zip
-move nasm-2.13.01\nasm.exe c:\nasm
+if not exist c:\nasm-2.13.01-win64.zip ( curl -o c:\nasm-2.13.01-win64.zip http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win64/nasm-2.13.01-win64.zip )
+7z x c:\nasm-2.13.01-win64.zip -oc:\
+dir /s /b c:\nasm-2.13.01
+set PATH=c:\nasm-2.13.01;%PATH%
 @echo off
