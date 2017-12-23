90cab81a892db5365d634801cdda18477591e11f
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-12-23 09:14:50 +0000

    Increase depth for appveyor.

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo on
-git submodule update --init --recursive --depth 100
+git submodule update --init --recursive --depth 1000
 if not exist c:\nasm-2.13.01-win64.zip ( curl -o c:\nasm-2.13.01-win64.zip http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win64/nasm-2.13.01-win64.zip )
 7z x c:\nasm-2.13.01-win64.zip -oc:\
 dir /s /b c:\nasm-2.13.01
