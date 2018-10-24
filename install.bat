cdaf993799548da5370a5b845054992dc7b69659
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2018-10-24 10:04:36 +0100

    Update nasm link.

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 @echo on
 git submodule update --init --recursive --depth 1000
-if not exist c:\nasm-2.13.01-win64.zip ( curl -o c:\nasm-2.13.01-win64.zip http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win64/nasm-2.13.01-win64.zip )
-7z x c:\nasm-2.13.01-win64.zip -oc:\
-dir /s /b c:\nasm-2.13.01
-set PATH=c:\nasm-2.13.01;%PATH%
+if not exist c:\nasm-2.13.03-win64.zip ( curl -o c:\nasm-2.13.03-win64.zip https://www.nasm.us/pub/nasm/releasebuilds/2.13.03/win64/nasm-2.13.03-win64.zip )
+7z x c:\nasm-2.13.03-win64.zip -oc:\
+dir /s /b c:\nasm-2.13.03
+set PATH=c:\nasm-2.13.03;%PATH%
 @echo off
