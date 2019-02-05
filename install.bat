91387f6ee316b5400b96b4a1f712a474a03783cb
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-02-05 14:04:20 +0000

    Bump nasm.

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 @echo on
 git submodule update --init --recursive --depth 1000
-if not exist c:\nasm-2.13.03-win64.zip ( curl -o c:\nasm-2.13.03-win64.zip https://www.nasm.us/pub/nasm/releasebuilds/2.13.03/win64/nasm-2.13.03-win64.zip )
-7z x c:\nasm-2.13.03-win64.zip -oc:\
-dir /s /b c:\nasm-2.13.03
-set PATH=c:\nasm-2.13.03;%PATH%
+if not exist c:\nasm-2.14.02-win64.zip ( curl -o c:\nasm-2.14.02-win64.zip https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/win64/nasm-2.14.02-win64.zip )
+7z x c:\nasm-2.14.02-win64.zip -oc:\
+dir /s /b c:\nasm-2.14.02
+set PATH=c:\nasm-2.14.02;%PATH%
 @echo off
