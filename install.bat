f7397bfda1a359e6097a4c142bf95c6760cc54c3
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-04-04 11:19:46 +0100

    Install patch to create lib files.

--- install.bat
+++ install.bat
@@ -1,5 +1,8 @@
 @echo on
 git submodule update --init --recursive --depth 1000
+pushd ffmpeg
+git apply ../create-lib-libraries.patch
+popd
 if not exist c:\nasm-2.14.02-win64.zip ( curl -o c:\nasm-2.14.02-win64.zip https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/win64/nasm-2.14.02-win64.zip )
 7z x c:\nasm-2.14.02-win64.zip -oc:\
 dir /s /b c:\nasm-2.14.02
