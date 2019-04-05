559b81be7f08320e292c93ac3a547221888cbf4d
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-04-05 13:16:45 +0100

    Fix date in tag script. [ci skip]

--- tag.sh
+++ tag.sh
@@ -1,4 +1,4 @@
 . functions.sh
-VERSION=$(get_git_date folder=$folder).$MINOR.$PATCH
+VERSION=$(get_git_date folder=ffmpeg).$MINOR.$PATCH
 echo "Tagging version $VERSION"
 git tag -a -m "Tagging version $VERSION" $VERSION
