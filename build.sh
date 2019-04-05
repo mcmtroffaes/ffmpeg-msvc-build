b44c52bd458b71f452b34e9d8be0bd98d6d985c7
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-04-05 12:59:11 +0100

    Ensure functions are imported.

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 # exit immediately upon error
 set -e
-
+cd $(cygpath "$APPVEYOR_BUILD_FOLDER")
 . functions.sh
-
 set -xe
 # bash starts in msys home folder, so first go to project folder
-cd $(cygpath "$APPVEYOR_BUILD_FOLDER")
 make_all \
 	license=${LICENSE,,} \
 	visual_studio=${TOOLSET,,} \
