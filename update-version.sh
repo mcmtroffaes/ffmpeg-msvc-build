97ea66d654ab0e3578b7665273bb50c32c8b0c39
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-04-10 14:34:18 +0100

    Fix update version script. [ci skip]

--- update-version.sh
+++ update-version.sh
@@ -3,7 +3,7 @@ pushd ffmpeg
 git fetch
 git checkout origin/master
 popd
-git commit -a -m "Update ffmpeg to version $(get_git_date folder=$folder)-$(get_git_hash folder=$folder)"
+git commit -a -m "Update ffmpeg to version $(get_git_date folder=ffmpeg)-$(get_git_hash folder=ffmpeg)"
 VERSION=$(get_git_date folder=ffmpeg).$MINOR.$PATCH
 echo "Tagging version $VERSION"
 git tag -a -m "Tagging version $VERSION" $VERSION
