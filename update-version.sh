981f355885cf9c9030c0c9b3d873b1e88df6d70e
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-04-12 09:52:47 +0100

    Let update-version.sh script use zeranoe hash (so same version is built as latest zeranoe build). [ci skip]

--- update-version.sh
+++ update-version.sh
@@ -1,7 +1,8 @@
 . functions.sh
+ZERANOE_FFMPEG_HASH=`curl https://ffmpeg.zeranoe.com/builds/ | grep -Po 'value="[0-9]+-\K[a-z0-9]+' | head -n 1`
 pushd ffmpeg
 git fetch
-git checkout origin/master
+git checkout $ZERANOE_FFMPEG_HASH
 popd
 git commit -a -m "Update ffmpeg to version $(get_git_date folder=ffmpeg)-$(get_git_hash folder=ffmpeg)"
 VERSION=$(get_git_date folder=ffmpeg).$MINOR.$PATCH
