d0ba90b9730a7dbd2531f2aab57b5b8cfed23f29
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-04-12 17:42:50 +0100

    Quit if already up to date. [ci skip]

--- update-version.sh
+++ update-version.sh
@@ -2,6 +2,11 @@
 ZERANOE_FFMPEG_HASH=`curl https://ffmpeg.zeranoe.com/builds/ | grep -Po 'value="[0-9]+-\K[a-z0-9]+' | head -n 1`
 pushd ffmpeg
 git fetch
+if [[ `git rev-parse HEAD` == $ZERANOE_FFMPEG_HASH* ]]
+then
+    echo "Already up to date."
+    exit 0
+fi
 git checkout $ZERANOE_FFMPEG_HASH
 popd
 git commit -a -m "Update ffmpeg to version $(get_git_date folder=ffmpeg)-$(get_git_hash folder=ffmpeg)"
