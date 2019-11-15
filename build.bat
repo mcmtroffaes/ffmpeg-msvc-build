1c2b3c0cd116bced8370c2bd0f78d4dc72e6263c
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-11-15 08:22:16 +0000

    Try to use vcpkg for build instead.

--- build.bat
+++ build.bat
@@ -1,18 +1,5 @@
 @echo on
-rem set up Visual Studio environment for specified toolset and platform
-rem see https://www.appveyor.com/docs/lang/cpp/
-rem and https://stackoverflow.com/a/46994531
-rem (differenes: no need for SDK, use native amd64 instead of x86_amd64)
-if "%TOOLSET%" == "v120" (
-  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
-  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
-)
-if "%TOOLSET%" == "v140" (
-  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-)
-if "%TOOLSET%" == "v141" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %PLATFORM% -vcvars_ver=14.1
-if "%TOOLSET%" == "v142" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %PLATFORM% -vcvars_ver=14.2
-rem run main build script
-C:\msys64\usr\bin\bash -lc "$APPVEYOR_BUILD_FOLDER/build.sh"
+vcpkg install ffmpeg:x64-windows
+vcpkg export ffmpeg:x64-windows --output=export --raw
+dir export\installed\x64-windows\ /s /b
 @echo off
