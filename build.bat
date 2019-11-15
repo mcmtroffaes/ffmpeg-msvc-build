e77f7e918970fbe1a65a2dcffe599b789b6383f2
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-11-15 08:34:05 +0000

    Just run everything from within appveyor.yml file.

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-vcpkg install ffmpeg:x64-windows
-vcpkg export ffmpeg:x64-windows --output=export --raw
-dir export\installed\x64-windows\ /s /b
-@echo off
--- install.bat
+++
@@ -1,6 +0,0 @@
-@echo on
-cd C:\Tools\vcpkg
-git pull
-.\bootstrap-vcpkg.bat
-cd %APPVEYOR_BUILD_FOLDER%
-@echo off
