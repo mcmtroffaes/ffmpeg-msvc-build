c4e9ee948dd96fa7a7018e7e4dc21b386b4c5083
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-11-17 13:02:56 +0000

    Update to latest ffmpeg version, also fix issue in update script.

--- update-version.ps1
+++ update-version.ps1
@@ -1,3 +1,13 @@
+# script to update VERSION and SHA512 files to the zeranoe ffmpeg version
+# to run this script, first set
+# $env:PSExecutionPolicyPreference = "Bypass" in your local session
+
+param (
+    [switch]$force = $false
+)
+
+$ErrorActionPreference = "Stop"
+
 $version_old = Get-Content "VERSION" -First 1 -Encoding Ascii
 $version_nuget_old, $version_hash_old = $version_old.Split("-")
 $version_nuget_major_old, $version_nuget_minor_old, $version_nuget_patch_old = $version_nuget_old.Split(".")
@@ -10,7 +20,7 @@ $match = $zeranoe | Select-String -Pattern "value=""([0-9]+-[a-z0-9]+)"""
 $zeranoe_version = $match.Matches[0].Groups[1].Value
 $version_nuget_major, $version_hash = $zeranoe_version.Split("-")
 
-if ($version_hash -eq $version_hash_old) {
+if (($version_hash -eq $version_hash_old) -and (-not $force)) {
   Write-Output "Already up to date."
 }
 else {
@@ -18,7 +28,10 @@ else {
   $sha512 = (Get-FileHash -Algorithm SHA512 "$env:temp\$version_hash.tar.gz").Hash.ToLower()
   Write-Output $sha512 | Set-Content "SHA512" -Encoding Ascii -NoNewline
   if ($version_nuget_major -eq $version_nuget_major_old) {
-    $version_nuget_minor = [convert]::ToInt64($version_nuget_minor_old) + 1;
+    $version_nuget_minor = [convert]::ToInt64($version_nuget_minor_old) + 1
+  }
+  else {
+    $version_nuget_minor = 0
   }
   Write-Output "new major: $version_nuget_major_old" "new minor: $version_nuget_minor" "new hash:  $version_hash"
   $version_nuget = "{0}.{1}.0" -f $version_nuget_major, $version_nuget_minor
