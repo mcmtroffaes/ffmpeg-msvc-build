f856778c383edb98c59447d740c90d1587189cd4
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-09-07 13:35:41 +0100

    Switch to standard vcpkg triplets.

--- build.ps1
+++ build.ps1
@@ -1,34 +1,8 @@
 param (
     [string]$vcpkg = "C:\Tools\vcpkg", 
-    [string]$platform = "x64", 
-    [string]$runtime_library = "MD",
-    [string]$linkage = "dynamic",
-    [string]$toolset = "v142",
+    [string]$triplet = "x64-windows",
     [string]$features = "core"
 )
-
-# create vcpkg triplet
-
-$platform = $platform.tolower()
-$runtime_library = $runtime_library.tolower()
-$linkage = $linkage.tolower()
-$toolset = $toolset.tolower()
-$features = $features.tolower()
-
-switch ($runtime_library) {
-  "md" { $crt_linkage = "dynamic" }
-  "mt" { $crt_linkage = "static" }
-  default { throw("invalid runtime library $runtime_library (expected MD or MT)")}
-}
-
-$triplet = "$platform-windows-$linkage-$runtime_library-$toolset"
-
-Write-Output `
-  "set(VCPKG_TARGET_ARCHITECTURE $platform)" `
-  "set(VCPKG_CRT_LINKAGE $crt_linkage)" `
-  "set(VCPKG_LIBRARY_LINKAGE $linkage)" `
-  "set(VCPKG_PLATFORM_TOOLSET $toolset)" `
-  | Out-File -FilePath "$vcpkg\triplets\$triplet.cmake" -Encoding ascii
 
 # update CONTROL and portfile.cmake files to the proper version
 
@@ -113,7 +87,7 @@ else {
   throw "unknown license"
 }
 
-$ffmpeg = "ffmpeg-$version-$license-$toolset-$linkage-$runtime_library-$platform"
+$ffmpeg = "ffmpeg-$version-$license-$triplet"
 
 # export
 
