393a3d1fbc1d31f5b54bee6252b1609349d1d67f
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-09-12 13:09:28 +0100

    Use cache, build vcpkg version rather than master to keep uptsream patching minimal.

--- build.ps1
+++ build.ps1
@@ -4,48 +4,14 @@ param (
     [string]$features = "core"
 )
 
-# update CONTROL and portfile.cmake files to the proper version
-
-$version = Get-Content "VERSION" -First 1 -Encoding Ascii
-$version_nuget, $version_hash = $version.Split("-")
-$version_nuget_major, $version_nuget_minor, $version_nuget_patch = $version_nuget.Split(".")
-$version_nuget_year  = [convert]::ToInt64($version_nuget_major.Substring(0, 4))
-$version_nuget_month = [convert]::ToInt64($version_nuget_major.Substring(4, 2))
-$version_nuget_day   = [convert]::ToInt64($version_nuget_major.Substring(6, 2))
-$version_vcpkg = "{0,4:d4}-{1,2:d2}-{2,2:d2}-{3}" -f $version_nuget_year, $version_nuget_month, $version_nuget_day, $version_nuget_minor
+# get version from CONTROL
 
 $control = Get-Content "$vcpkg\ports\ffmpeg\CONTROL"
-if (-Not $control[1].StartsWith("Version:")) { throw "could not find version field in CONTROL file" }
-$control[1] = "Version: $version_vcpkg"
-$control -join "`n" | Set-Content "$vcpkg\ports\ffmpeg\CONTROL" -Encoding Ascii -NoNewline
-Write-Output "" "CONTROL" "~~~~~~~" "" $control[0..4]
-
-$sha512 = Get-Content "SHA512" -First 1 -Encoding Ascii
-
-$portfile = Get-Content "$vcpkg\ports\ffmpeg\portfile.cmake"
-if (-Not $portfile[3].StartsWith("    REF")) {
-  Write-Output "" "portfile.cmake" "~~~~~~~~~~~~~~" "" $portfile[0..20] ""
-  throw "could not find REF field in portfile"
-}
-if (-Not $portfile[4].StartsWith("    SHA512")) {
-  Write-Output "" "portfile.cmake" "~~~~~~~~~~~~~~" "" $portfile[0..20] ""
-  throw "could not find SHA512 field in portfile"
-}
-$portfile[3] = "    REF $version_hash"
-$portfile[4] = "    SHA512 $sha512"
-$portfile -join "`n" ` | Set-Content "$vcpkg\ports\ffmpeg\portfile.cmake" -Encoding Ascii -NoNewline
-Write-Output "" "portfile.cmake" "~~~~~~~~~~~~~~" "" $portfile[0..12] ""
-
-# fix patches
-Copy-Item -Path "0006-fix-StaticFeatures.patch" -Destination "$vcpkg\ports\ffmpeg\"
-Copy-Item -Path "0011-Fix-x265-detection.patch" -Destination "$vcpkg\ports\ffmpeg\"
-Copy-Item -Path "0012-Fix-ssl-110-detection.patch" -Destination "$vcpkg\ports\ffmpeg\"
-
-# update vcpkg_acquire_msys to use the appveyor version of msys64
-
-if ($env:APPVEYOR) {
-  Copy-Item -Path "vcpkg_acquire_msys.cmake" -Destination "$vcpkg\scripts\cmake\"
-}
+if (-Not $control[1].StartsWith("Version:")) { throw "could not find Version field in CONTROL file" }
+$control = Get-Content "$vcpkg\ports\ffmpeg\CONTROL"
+if (-Not $control[2].StartsWith("Port-Version:")) { throw "could not find Port-Version field in CONTROL file" }
+$version = $control[1].Remove(0, 9), $control[2].Remove(0,14) -Join "-"
+Write-Output "version: $version"
 
 # install
 
