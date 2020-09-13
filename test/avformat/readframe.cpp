c0bd9f1900771448b8e2ea192e46a0f242adae7a
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-09-13 13:25:26 +0100

    Give some extra logging info on readframe test, and free context.

--- test/avformat/readframe.cpp
+++ test/avformat/readframe.cpp
@@ -4,6 +4,7 @@
 extern "C" {
 #define __STDC_CONSTANT_MACROS
 #include <libavformat/avformat.h>
+#include <libavutil/log.h>
 }
 
 int main(int argc, char **argv)
@@ -12,16 +13,21 @@ int main(int argc, char **argv)
 		printf("expected one argument");
 		return -1;
 	}
+	av_log_set_callback(av_log_default_callback);
+	av_log_set_level(AV_LOG_DEBUG);
 	AVFormatContext *s = NULL;
 	AVPacket pkt = {0};
 	if (avformat_open_input(&s, argv[1], NULL, NULL) < 0) {
 		printf("failed to open input\n");
+		avformat_free_context(s);
 		return -1;
 	}
 	av_init_packet(&pkt);
 	if (av_read_frame(s, &pkt) < 0) {
 		printf("failed to read frame\n");
+		avformat_free_context(s);
 		return -1;
 	}
+	avformat_free_context(s);
 	return 0;
 }
