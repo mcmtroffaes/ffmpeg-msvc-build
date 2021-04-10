b285de7c9a18f66c7b79d72f9f3b655f4b02398e
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2021-04-10 09:09:05 +0100

    Add test program to list all devices.

---
+++ test/avdevice/list_all_devices.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <functional>
+
+extern "C" {
+#define __STDC_CONSTANT_MACROS
+#include <libavdevice/avdevice.h>
+}
+
+template <typename T>
+void list_devices(T*(*func_next)(T*))
+{
+	T* format = func_next(nullptr);
+	while (format) {
+		std::cout << format->name << std::endl;
+		format = func_next(format);
+	};
+}
+
+int main()
+{
+	avdevice_register_all();
+	std::cout << "audio input devices:" << std::endl;
+	list_devices(av_input_audio_device_next);
+	std::cout << "video input devices:" << std::endl;
+	list_devices(av_input_video_device_next);
+	std::cout << "audio output devices:" << std::endl;
+	list_devices(av_output_audio_device_next);
+	std::cout << "video output devices:" << std::endl;
+	list_devices(av_output_video_device_next);
+	return 0;
+}
