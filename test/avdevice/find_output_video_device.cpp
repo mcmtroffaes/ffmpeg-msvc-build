8ed2ee829941889745f91e76b82d71a5fcb2027a
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2021-04-10 09:18:55 +0100

    Add program to find output video device by name.

---
+++ test/avdevice/find_output_video_device.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <functional>
+#include "../log.h"
+
+extern "C" {
+#define __STDC_CONSTANT_MACROS
+#include <libavdevice/avdevice.h>
+}
+
+template <typename T>
+bool find_device(T*(*func_next)(T*), std::string name)
+{
+	T* format = func_next(nullptr);
+	while (format) {
+		if (name == format->name) return true;
+		format = func_next(format);
+	};
+	return false;
+}
+
+int main(int argc, char** argv)
+{
+	av_log_set_callback(av_log_default_callback);
+	av_log_set_level(AV_LOG_DEBUG);
+	if (argc != 2) {
+		logger::error() << "expected one argument";
+		return -1;
+	}
+
+	avdevice_register_all();
+	if (!find_device(av_output_video_device_next, argv[1])) {
+		logger::error() << "output video device " << argv[1] << " not found";
+		return -1;
+	};
+	logger::info() << "output video device " << argv[1] << " found";
+	return 0;
+}
