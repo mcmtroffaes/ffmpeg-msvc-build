1356f81b7084422b3191b7b89cc93cd305b18cfe
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2021-04-25 14:02:16 +0100

    small refactor [skip appveyor] [skip travis]

---
+++ .circleci/test.bat
@@ -0,0 +1,13 @@
+@echo on
+call "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars64.bat"
+set
+mkdir C:/Users/circleci/project/build-rel
+cd C:/Users/circleci/project/build-rel
+cmake C:/Users/circleci/project/test -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DFEATURES=%ALL_FEATURES% -DCMAKE_MSVC_RUNTIME_LIBRARY=%MSVC_RUNTIME_LIBRARY%
+cmake --build .
+ctest -V
+mkdir C:/Users/circleci/project/build-dbg
+cd C:/Users/circleci/project/build-dbg
+cmake C:/Users/circleci/project/test -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DFEATURES=%ALL_FEATURES% -DCMAKE_MSVC_RUNTIME_LIBRARY=%MSVC_RUNTIME_LIBRARY%
+cmake --build .
+ctest -V
