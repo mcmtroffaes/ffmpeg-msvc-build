0c6993157d09d107093d1492da8a3f9b268092b6
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2021-04-25 14:08:09 +0100

    further refactor [skip appveyor] [skip travis]

--- .circleci/test.bat
+++ .circleci/test.bat
@@ -1,13 +1,12 @@
 @echo on
-call "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars64.bat"
-set
-mkdir C:/Users/circleci/project/build-rel
-cd C:/Users/circleci/project/build-rel
-cmake C:/Users/circleci/project/test -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DFEATURES=%ALL_FEATURES% -DCMAKE_MSVC_RUNTIME_LIBRARY=%MSVC_RUNTIME_LIBRARY%
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+mkdir %HOMEDRIVE%%HOMEPATH%\build-rel
+cd %HOMEDRIVE%%HOMEPATH%\build-rel
+cmake %HOMEDRIVE%%HOMEPATH%\project\test -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_TOOLCHAIN_FILE=%HOMEDRIVE%%HOMEPATH%\project\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DFEATURES=%ALL_FEATURES% -DCMAKE_MSVC_RUNTIME_LIBRARY=%MSVC_RUNTIME_LIBRARY%
 cmake --build .
 ctest -V
-mkdir C:/Users/circleci/project/build-dbg
-cd C:/Users/circleci/project/build-dbg
-cmake C:/Users/circleci/project/test -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DFEATURES=%ALL_FEATURES% -DCMAKE_MSVC_RUNTIME_LIBRARY=%MSVC_RUNTIME_LIBRARY%
+mkdir %HOMEDRIVE%%HOMEPATH%\build-dbg
+cd %HOMEDRIVE%%HOMEPATH%\build-dbg
+cmake %HOMEDRIVE%%HOMEPATH%\project\test -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_TOOLCHAIN_FILE=%HOMEDRIVE%%HOMEPATH%\project\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DFEATURES=%ALL_FEATURES% -DCMAKE_MSVC_RUNTIME_LIBRARY=%MSVC_RUNTIME_LIBRARY%
 cmake --build .
 ctest -V
