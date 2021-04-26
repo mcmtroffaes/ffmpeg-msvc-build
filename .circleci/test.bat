c97624fc5ca0231cb95337ce4ab43cf21e08f4f4
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2021-04-26 15:17:50 +0100

    Update test.bat

--- .circleci/test.bat
+++ .circleci/test.bat
@@ -1,12 +1,12 @@
 @echo on
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-mkdir %HOMEDRIVE%%HOMEPATH%\build-rel
-cd %HOMEDRIVE%%HOMEPATH%\build-rel
-cmake %HOMEDRIVE%%HOMEPATH%\project\test -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_TOOLCHAIN_FILE=%HOMEDRIVE%%HOMEPATH%\project\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DFEATURES=%ALL_FEATURES% -DCMAKE_MSVC_RUNTIME_LIBRARY=%MSVC_RUNTIME_LIBRARY%
+mkdir %~dp0\build-rel
+cd %~dp0\build-rel
+cmake %~dp0\test -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_TOOLCHAIN_FILE=%~dp0\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DFEATURES=%ALL_FEATURES% -DCMAKE_MSVC_RUNTIME_LIBRARY=%MSVC_RUNTIME_LIBRARY%
 cmake --build .
 ctest -V
-mkdir %HOMEDRIVE%%HOMEPATH%\build-dbg
-cd %HOMEDRIVE%%HOMEPATH%\build-dbg
-cmake %HOMEDRIVE%%HOMEPATH%\project\test -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_TOOLCHAIN_FILE=%HOMEDRIVE%%HOMEPATH%\project\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DFEATURES=%ALL_FEATURES% -DCMAKE_MSVC_RUNTIME_LIBRARY=%MSVC_RUNTIME_LIBRARY%
+mkdir %~dp0\build-dbg
+cd %~dp0\build-dbg
+cmake %~dp0\test -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_TOOLCHAIN_FILE=%~dp0\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DFEATURES=%ALL_FEATURES% -DCMAKE_MSVC_RUNTIME_LIBRARY=%MSVC_RUNTIME_LIBRARY%
 cmake --build .
 ctest -V
