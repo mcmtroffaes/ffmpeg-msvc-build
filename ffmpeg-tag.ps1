ea874db358bb934524d267f881f266d9a50c103c
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2021-05-28 13:08:55 +0100

    Fix tag script.

--- ffmpeg-tag.ps1
+++ ffmpeg-tag.ps1
@@ -4,11 +4,11 @@
 $portfile = Get-Content "vcpkg\ports\ffmpeg\portfile.cmake"
 if (-Not $portfile[3].StartsWith("    REF")) { throw "could not find REF field in portfile" }
 $version_hash = $portfile[3].Substring(8)
-$control = Get-Content "vcpkg\ports\ffmpeg\CONTROL"
-if (-Not $control[1].StartsWith("Version:")) { throw "could not find Version field in CONTROL file" }
-if (-Not $control[2].StartsWith("Port-Version:")) { throw "could not find PortVersion field in CONTROL file" }
-$version = $control[1].Split(" ")[1]
-$port_version = $control[2].Split(" ")[1]
+$control = Get-Content "vcpkg\ports\ffmpeg\vcpkg.json"
+if (-Not $control[2].StartsWith("  ""version-string"":")) { throw "could not find version field in vcpkg.json file" }
+if (-Not $control[3].StartsWith("  ""port-version"":")) { throw "could not find port-version field in vcpkg.json file" }
+$version_old = $control[2].Split(":")[1].Trim(" "",")
+$port_version_old = $control[3].Split(":")[1].Trim(" "",")
 $tag = $version.Replace("-", "") + ".$port_version.0"
 Write-Output "Tagging version $tag ($version_hash)."
 & git tag -a -m "Tagging version $tag ($version_hash)." $tag
